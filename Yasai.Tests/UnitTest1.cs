11ef402ab900350c0f592973c7ab157bee802fcb
EpicTofuu <rinkaandchan@outlook.com>
2021-10-07 23:37:00 +1100

    added xunit tests

---
+++ Yasai.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Yasai.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(3 == 3);
+        }
+    }
+}
