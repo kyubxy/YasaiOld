b591c44800ec2527ae82db4e0282e85cb9d98a77
caveguy427 <46781184+caveguy427@users.noreply.github.com>
2021-10-09 21:39:10 +0800

    remove `Constants`, `.TinyFont` move to `SpriteFont`

--- Yasai/Constants.cs
+++
@@ -1,9 +0,0 @@
-namespace Yasai
-{
-    internal static class Constants
-    {
-        // this might be bad practice..
-        
-        public static string TinyFont => "yasai_fontTiny";
-    }
-}
