b626ff92f3dba1a6d0eb09b9a12fd5b323f9aad3
EpicTofu <rinkaandchan@outlook.com>
2021-12-29 17:21:11 +1100

    idk

--- Yasai/Resources/Stores/FontStore.cs
+++ Yasai/Resources/Stores/FontStore.cs
@@ -1,36 +1,28 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
+using SharpFNT;
 using Yasai.Graphics.Text;
 using Yasai.Structures.DI;
-using SixLabors.ImageSharp;
 
 namespace Yasai.Resources.Stores
 {
-    /*
     public class FontStore : ContentStore<SpriteFont>
     {
         public override string[] FileTypes => new[] { ".ttf", ".otf" };
         public override IResourceArgs DefaultArgs => new FontArgs(32);
 
-        private FontCollection collection;
-        
         public FontStore(DependencyContainer container, string root = "Assets") : base(container, root)
         {
-            collection = new FontCollection();
         }
 
         protected override SpriteFont AcquireResource(string path, IResourceArgs args)
         {
-            var fargs = (FontArgs) args;
-
-            FontFamily family = collection.Install(path);
-            Font font = family.CreateFont(fargs.Size, FontStyle.Regular); // TODO: fontstyle
-
-            return new SpriteFont(font, fargs, path);
+            throw new Exception();
+            //return new SpriteFont(font, fargs, path);
         }
     }
     
-    */
-    
+    // might not need args
     public class FontArgs : IResourceArgs
     {
         public int Size { get; } = 31;
