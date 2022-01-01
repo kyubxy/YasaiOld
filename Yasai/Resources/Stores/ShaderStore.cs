e849ced5928934a86af6b7ef07b57936ec8a38f9
EpicTofu <rinkaandchan@outlook.com>
2022-01-01 19:48:43 +1100

    removed VisualTests namespace

--- Yasai/Resources/Stores/ShaderStore.cs
+++ Yasai/Resources/Stores/ShaderStore.cs
@@ -40,10 +40,5 @@ namespace Yasai.Resources.Stores
 
             return new Shader(Path.Combine(Root, fragPath, frag), Path.Combine(Root, vertPath, vert));
         }
-
-        public override Shader GetResource(string res)
-        {
-            return base.GetResource(res);
-        }
     }
 }
