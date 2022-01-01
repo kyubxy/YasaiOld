470aa669019b3c43b2dcaa15396241a6fa2927cd
EpicTofu <rinkaandchan@outlook.com>
2022-01-02 01:07:53 +1100

    fixed tests

--- Yasai.Tests/Graphics/DrawableTest.cs
+++ Yasai.Tests/Graphics/DrawableTest.cs
@@ -9,19 +9,19 @@ namespace Yasai.Tests.Graphics
         [Fact]
         void testAnchors()
         {
-            var tests = new Vector2i[]
+            var tests = new Vector2[]
             {
-                new( 1,  1), // 0 topleft
-                new( 0,  1), // 1 top
-                new(-1,  1), // 2 topright
+                new(0, 0), // 0 topleft
+                new(0.5f, 0), // 1 top
+                new(1, 0), // 2 topright
                 
-                new( 1,  0), // 3 left
-                new( 0,  0), // 4 middle
-                new(-1,  0), // 5 right
+                new(0,0.5f), // 3 left
+                new(0.5f,0.5f), // 4 middle
+                new(1,0.5f), // 5 right
                 
-                new( 1, -1), // 6 bottomleft
-                new( 0, -1), // 7 bottom
-                new(-1, -1), // 8 bottomright
+                new(0, 1), // 6 bottomleft
+                new(0.5f, 1), // 7 bottom
+                new(1, 1), // 8 bottomright
             };
             
             for (int i = 0; i < tests.Length; i++)
