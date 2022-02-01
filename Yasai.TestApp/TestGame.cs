861dda8cf783a309fee990dce48eb8a8fa1b4bb8
Justin <rinkaandchan@outlook.com>
2022-02-01 19:31:29 +1100

    rotation + parent-centric position + origin

--- Yasai.TestApp/TestGame.cs
+++ Yasai.TestApp/TestGame.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
+using Yasai.Graphics;
 using Yasai.Graphics.Shapes;
 using Yasai.Structures.DI;
 
@@ -9,23 +10,25 @@ namespace Yasai.TestApp
 {
     public class TestGame : Game
     {
-        private Box box;
-        private Box box2;
+        private Box child;
+        private Box parent;
         private float rot;
         private Vector2 pos;
 
     public override void Load(DependencyContainer dependencies)
         {
             base.Load(dependencies);
-            Root.Add(box2 = new Box
+            Root.Add(parent = new Box
             {
                 Size = new Vector2(200),
-                Colour = Color.MediumSpringGreen
+                Colour = Color.MediumSpringGreen,
+                Origin = Anchor.TopLeft,
+                Position = new Vector2(300)
             });
-            Root.Add(box = new Box
+            Root.Add(child = new Box
             {
-                Size = new Vector2(200),
-                Colour = Color.Red
+                Colour = Color.Red,
+                Position = new Vector2(0.25f)
             });
         }
 
@@ -35,22 +38,17 @@ namespace Yasai.TestApp
             base.Update(args);
             s += 0.01f;
 
-            box.Position += new Vector2(0.01f);
-            box.Rotation -= 0.01f;
+            child.Rotation = s;
+            child.X += 0.01f;
             
-            box.p =
-                Matrix4.CreateTranslation(1,-1,0) * 
+            child.p =
+                Matrix4.CreateTranslation(1,1,0) * 
                 Matrix4.CreateRotationZ(s) *
                 Matrix4.CreateScale(new Vector3(80)) *
                 Matrix4.CreateTranslation(300,300,0) *
                 Matrix4.Identity;
-            
-            box2.p =
-                Matrix4.CreateTranslation(1,-1,0) * 
-                Matrix4.CreateRotationZ(s) *
-                Matrix4.CreateScale(new Vector3(100)) *
-                Matrix4.CreateTranslation(300,300,0) *
-                Matrix4.Identity;
+
+            parent.Rotation = s;
         }
     }
 }
